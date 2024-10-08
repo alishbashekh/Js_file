c90bd50fae98fb2ab328a96bcc6350c4a7729ca6
alishbashekh <alishbashabbir890@gmail.com>
2024-10-08 17:53:25 +0500

    datatype summary

---
+++ primitive & non-primitive.js
@@ -0,0 +1,8 @@
+// primitive datatype
+// string, number, null, bolean, undefined, symbol, bigint.
+
+// refrence or non-primitive : allocates refrence in memory.
+// arrays, objects, funcions.
+const hello=123;
+
+console.log(typeof hello);
