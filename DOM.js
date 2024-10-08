f9d6db3a1b78c3c0ae2d5b3b07877b160f8c1591
alishbashekh <alishbashabbir890@gmail.com>
2024-10-08 19:02:55 +0500

    .

---
+++ DOM.js
@@ -0,0 +1,8 @@
+let myarr=[1,2,3,4,5,6,5]
+ 
+function jk(){
+    for(i=0;i<myarr.length;i++){
+        document.write(myarr[i]);
+    }
+}
+ document.getElementById('ar').innerHTML=('myarr[i]');
---
+++ arrays.js
@@ -0,0 +1,18 @@
+const myarr=[1,3,4,5,6,7]
+// document.write(myarr[3]);
+// array method
+
+// myarr.push(4);***********stores value in nth index
+// myarr.push(6)
+// myarr.pop()
+// document.write(myarr);
+
+// myarr.unshift(9) *********stores value in oth index
+// document.write(myarr);
+// document.write(myarr);
+
+
+// const mya= myarr.slice(1,4); *********does not include last given index
+//  document.write(mya);
+
+//  const myb=myarr.splice(1,4);********* does include last given index
